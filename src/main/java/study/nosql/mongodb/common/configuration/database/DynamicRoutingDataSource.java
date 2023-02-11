d0eadc5dd96cecdf082139b397fc6d87869b9fe3
devjun10 <devjun1023@gmail.com>
2023-02-12 00:57:33 +0900

    build: 멀티 데이터베이스 설정 추가
    - MySQL NamedLock을 사용하기 위한 설정 추가

---
+++ src/main/java/study/nosql/mongodb/common/configuration/database/DynamicRoutingDataSource.java
@@ -0,0 +1,13 @@
+package study.nosql.mongodb.common.configuration.database;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import static org.springframework.transaction.support.TransactionSynchronizationManager.isCurrentTransactionReadOnly;
+
+public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return isCurrentTransactionReadOnly() ? "replication" : "master";
+    }
+}
